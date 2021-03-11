aeb5618ba3ff52e097582a01a76b31fee8c06bea
DESKTOP-L56QVGG\Эльвир <mardanovelvir@gmail.com>
2021-03-11 14:41:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CppAndCs
+{
+    class Program
+    {
+        public const string Project1Dll = @"..\..\..\Debug\CppFunctions.dll"; // gставить свой путь к dll
+        [DllImport(Project1Dll, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int qsortRecursive(int[] a, int n);
+
+        static void Main(string[] args)
+        {
+            int[] output1 = new int[4] { 1, 4, 2, 3 };
+            int output2 = 4;
+            qsortRecursive(output1, output2);
+            for (int i = 0; i < output1.Length; i++)
+            {
+                Console.Write(output1[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
